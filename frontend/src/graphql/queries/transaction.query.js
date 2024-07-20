5bb3be744676a6d8a5adbd8ffcd3062db11a4743
Sahil Prasad <sahilprasad41969@gmailcom>
2024-07-20 13:09:48 +0530

    transaction CRUD done

--- frontend/src/graphql/queries/transaction.query.js
+++ frontend/src/graphql/queries/transaction.query.js
@@ -33,3 +33,13 @@ export const GET_TRANSACTION = gql`
 		}
 	}
 `;
+
+
+export const GET_TRANSACTION_STATISTICS = gql`
+	query GetTransactionStatistics {
+		categoryStatistics {
+			category
+			totalAmount
+		}
+	}
+`;
